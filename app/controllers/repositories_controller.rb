08b4ee55f0a6cf30a4ddbe0eb739fcc0b4cbfead
Eric McKinney <mckinney99@gmail.com>
2018-03-13 15:32:56 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -10,9 +10,6 @@ class RepositoriesController < ApplicationController
         req.params['client_id'] = '303c70dffe3dc17fc725'
         req.params['client_secret'] = '8a1198411520e1aef83dc8c30c0db6303d5d9946'
         req.params['q'] = params[:query]
-        req.params['search'] = params[:repositories]
-        req.params['query'] = 'repositories'
-        req.options.timeout = 0
       end
       body = JSON.parse(@resp.body)
       if @resp.success?
