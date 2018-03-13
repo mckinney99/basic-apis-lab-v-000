68f3a3efcc29f1f134ee20d07d9101b473eb6789
Eric McKinney <mckinney99@gmail.com>
2018-03-13 15:23:34 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -16,7 +16,7 @@ class RepositoriesController < ApplicationController
       end
       body = JSON.parse(@resp.body)
       if @resp.success?
-        @venues = body["response"]["venues"]
+        @repositories = body["response"]["repositories"]
       else
         @error = body["meta"]["errorDetail"]
       end
